0800f7f18f915ceba6ebe54b1713cb6617ce471f
Andreu Moreno i Vendrell <amoreno@euss.cat>
2020-07-10 09:48:22 +0200

    Colomap

--- GenerationAgentsDistribution.R
+++ GenerationAgentsDistribution.R
@@ -2,8 +2,8 @@ df <- read.table("position_agents_norep_clean.data")
 
 require(ggplot2)
 
-p <- ggplot(df, aes(df$V1,df$V2)) + labs(x = "X") + labs(y = "Y") + labs(title = "Distribució agents DMASON")
-p <- p + stat_bin2d(bins = 20)
+p <- ggplot(df, aes(df$V1,df$V2)) + labs(x = "X") + labs(y = "Y") + labs(title = "DMASON agent distribution")
+p <- p + stat_bin2d(bins = 20) + scale_fill_gradient(low = "lightblue", high = "red")
 pdf("AgentsDistribDMASON.pdf")
 plot(p)
 dev.off()
